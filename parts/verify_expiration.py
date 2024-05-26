3dee9ec7a8431ceea50e11d5ae4f1d68865b9416
MASAMI Mashino <mashi.zzz@gmail.com>
2024-05-27 08:03:41 +0900

    refuctoring and add comment

--- parts/verify_expiration.py
+++ parts/verify_expiration.py
@@ -6,12 +6,6 @@ import sys
 
 # 将来的には、QThreadを使ってGUIでも読み取り中に画面がロックしないようにする。
 
-# Suica待ち受けの1サイクル秒
-TIME_cycle = 0.5
-# Suica待ち受けの反応インターバル秒
-TIME_interval = 0.2
-# タッチされてから次の待ち受けを開始するまで無効化する秒
-TIME_wait = 1.5
 
 
 
@@ -34,10 +28,10 @@ def led_on(COLOR):
     """
     LEDはラズパイにしかないので、plathomeにてMacもしくはラズパイの判定を行って
     処理を分岐している。
-    Macの場合は、コンソールに表示するだけ。
     """
     import platform
     osname = platform.system()
+    # Macの場合は、コンソールに表示するだけ。
     if osname == 'Darwin':
         if COLOR == 'RED':
             print('RED ON')
@@ -45,7 +39,7 @@ def led_on(COLOR):
             print('BLUE ON')
         elif COLOR == 'YELLOW':
             print('YELLOW ON')
-
+    # Raspberry Piの場合は、GPIO_NUMを設定してLEDを点灯する。
     elif osname == 'Linux':
         if COLOR == 'RED':
             GPIO_NUM = 16
@@ -64,28 +58,41 @@ def led_on(COLOR):
             pi.write(GPIO_NUM, 0)
             pi.set_mode(GPIO_NUM, pigpio.INPUT)
             pi.stop()
-        except KeyboardInterrupt:  # Ctrl+Cを押すとループを抜ける
+        # Macの場合も、エラーをキャッチして実行するようにすると、コードが短くなるかも。
+        # エラー前提というのは、少々気持ち悪いけど。
+        # except KeyboardInterrupt:  # Ctrl+Cを押すとループを抜ける
+        finally:
             pass
 
 
 def on_connect(tag):
     
     import pandas as pd
+    # 照合用のデータは、エクセルで作成
+    # 照合用のデータをdfに読み込み
     df = pd.read_excel('data.xlsx', dtype=str)
 
-    #タッチ時の処理
-    #IDmのみ取得して表示
-    idm = binascii.hexlify(tag.idm).decode()
-    idm = str(idm)
+    # タッチ時の処理
+    # フルの情報を引き出す時は、以下のコード。
     #idm = binascii.hexlify(tag._nfcid)
-    print("IDm : " + idm)
+    # IDmのみ取得して表示
+    idm = binascii.hexlify(tag.idm).decode()
+    idm = str(idm) # 念の為に str にする。
+    print("IDm : " + idm) # 照合結果を表示
     
+    # 有効期限と、本日を比較を行う
     from datetime import datetime, date, timedelta
+    
+    # Idmと一致する行を抽出
     print(df[df['Idm'].isin([idm])])
+    
+    # 日付の比較用に文字列を日付に変換。
+    # 日付だけで良いので、dateを使用する。
     s = df[df['Idm'].isin([idm])]['Expiration'].values.tolist()[0]
     s_format = '%Y-%m-%d %H:%M:%S'
+    
+    # 比較用の有効期限
     expiration_date = datetime.strptime(str(s), s_format).date()
-    #print(expiration_date)
 
     #print(dt,type(dt))
     from dateutil.relativedelta import relativedelta
@@ -94,7 +101,15 @@ def on_connect(tag):
     today = date.today()
     after_six_months = today + relativedelta(years=0, months=6, days=-1) # 2019-05-31
     
+    
+
     def discriminate():
+        """
+        有効期限に従って、表示する色を返す。
+        赤：期限切れ
+        黄：６ヶ月以内
+        青：６ヶ月以上
+        """
         if (today > expiration_date):
             return 'RED'
         elif (after_six_months > expiration_date):
@@ -102,7 +117,9 @@ def on_connect(tag):
         else:
             return 'BLUE'
 
+    # 期限を判定し、色を取得
     COLOR = discriminate()
+    # 取得した色をled_onへ渡す。
     led_on(COLOR)
     #print(name)
     return True
@@ -110,18 +127,27 @@ def on_connect(tag):
 
 
 def read_id():
+    # あまり美しくないけど、グローバル変数に。
+    # クラス化して実装する方が良さそう。
     global clf
+    # USBに接続されたNFCリーダに接続してインスタンス化
     clf = nfc.ContactlessFrontend('usb')
     try:
+        # on_connectを呼び出し。呼び出しの間隔、時間は以下の通り。
+        # Suica待ち受けの1サイクル秒
+        TIME_cycle = 0.5
+        # Suica待ち受けの反応インターバル秒
+        TIME_interval = 0.2
         clf.connect(rdwr={'on-connect': on_connect}, iterations=int(TIME_cycle//TIME_interval)+1 , interval=TIME_interval)
     finally:
         clf.close()
  
-
 def main():
+    # タッチされてから次の待ち受けを開始するまで無効化する秒
+    TIME_wait = 1.5
+    # アプリ起動時に表示
     print('カードをタッチしてください')
 
-# USBに接続されたNFCリーダに接続してインスタンス化
     try:
         while True:
             read_id()
@@ -129,7 +155,8 @@ def main():
             time.sleep(TIME_wait)
             print('カードをタッチしてください')
             #end if
-
+    
+    # Ctrl-Cで終了できるようにする。
     except KeyboardInterrupt:
         clf.close()
         print(' Catch Ctrl-C. 終了します。')
